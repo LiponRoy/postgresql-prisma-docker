a23f077eb3cdf0c9364ad0a812090af8a6795d6c
lipon <liponbdbd@gmail.com>
2025-10-28 18:29:06 +0600

    created notfound and validateRequest middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cors from 'cors'
 import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 import { userRoutes } from "./modules/user/user.routes";
 import cookieParser from 'cookie-parser';
+import notFound from "./middlewares/notFound";
 
 const app = express();
 app.use(express.json());
@@ -18,18 +19,10 @@ const prisma = new PrismaClient();
 app.get("/", (req, res) => res.send(" Express + Prisma + Docker + TypeScript"));
 app.use('/api/v1/user', userRoutes);
 
-// app.get("/user", async (req, res) => {
-//    const user = await prisma.user.findMany();
-//   res.json(user);
-// });
-
-// app.post("/user", async (req, res) => {
-//   const { name,email,password} = req.body;
-//   const user = await prisma.user.create({ data: { name,email,password} });
-//   res.json(user);
-// });
-
 // Error handler
 app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
 
 export default app;
